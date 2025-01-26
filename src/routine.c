c69ac835a367d7b49eff5ad6ae6a42e50f04f664
gongarci <gongarci@gmail.com>
2025-01-26 02:39:54 +0100

    apparetly everything working ,  ready for reviews

--- src/routine.c
+++ src/routine.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   routine.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: gongarci <gongarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 23:57:47 by marvin            #+#    #+#             */
-/*   Updated: 2025/01/25 23:25:56 by marvin           ###   ########.fr       */
+/*   Updated: 2025/01/26 02:38:20 by gongarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,24 @@
 
 static int	sleeping(t_philo *philos)
 {
-	if (!check_philo(philos))
-		return (0);
+	pthread_mutex_lock(philos->print);
 	print_thread(philos, philos->id, PHILO_SLEEP);
+	pthread_mutex_unlock(philos->print);
 	//printf("time to sleep + current %d >= %d \n", philos->time_to_sleep + (int)get_time(), philos->time_to_die);
-	if (philos->time_to_sleep + get_time() >= philos->time_to_die)
+/* 	if (philos->time_to_sleep + get_time() >= philos->time_to_die)
 	{
 		ft_usleep(philos->time_to_die - get_time());
 	}
 	else
-		ft_usleep(philos->time_to_sleep);
+		ft_usleep(philos->time_to_sleep); */
+	ft_usleep(philos->time_to_sleep);
 	return (1);
 }
 
 void	take_forks(t_philo *philos)
 {
-	if (philos->id % 2 == 0 && philos->nb_philos % 2)
+	if (philos->id % 2 == 1)
 	{
-		usleep(100);
 		pthread_mutex_lock(philos->left_fork);
 		pthread_mutex_lock(philos->print);
 		print_thread(philos, philos->id, PHILO_TAKE_FORK);
@@ -45,21 +45,50 @@ void	take_forks(t_philo *philos)
 	}
 	else
 	{
-		usleep(100);
 		pthread_mutex_lock(philos->right_fork);
+		pthread_mutex_lock(philos->print);
 		print_thread(philos, philos->id, PHILO_TAKE_FORK);
+		pthread_mutex_unlock(philos->print);
 		printf("%p\n", philos->right_fork);
 		pthread_mutex_lock(philos->left_fork);
+		pthread_mutex_lock(philos->print);
 		print_thread(philos, philos->id, PHILO_TAKE_FORK);
+		pthread_mutex_unlock(philos->print);
 		printf("%p\n", philos->left_fork);
 	}
+/* 	if (philos->id % 2 == 0 && philos->nb_philos % 2)
+	{
+		pthread_mutex_lock(philos->right_fork);
+		pthread_mutex_lock(philos->print);
+		print_thread(philos, philos->id, PHILO_TAKE_FORK);
+		pthread_mutex_unlock(philos->print);
+		printf("%p\n", philos->right_fork);
+		pthread_mutex_lock(philos->left_fork);
+		pthread_mutex_lock(philos->print);
+		print_thread(philos, philos->id, PHILO_TAKE_FORK);
+		pthread_mutex_unlock(philos->print);
+		printf("%p\n", philos->left_fork);
+
+	}
+	else
+	{
+		pthread_mutex_lock(philos->left_fork);
+		pthread_mutex_lock(philos->print);
+		print_thread(philos, philos->id, PHILO_TAKE_FORK);
+		pthread_mutex_unlock(philos->print);
+		printf("%p\n", philos->left_fork);
+		pthread_mutex_lock(philos->right_fork);
+		pthread_mutex_lock(philos->print);
+		print_thread(philos, philos->id, PHILO_TAKE_FORK);
+		pthread_mutex_unlock(philos->print);
+		printf("%p\n", philos->right_fork);
+	} */
 }
 
 void	release_forks(t_philo *philos)
 {
-	if (philos->id % 2 == 0  && philos->nb_philos % 2)
+	if (philos->id % 2 == 1)
 	{
-		//usleep(100);
 		pthread_mutex_unlock(philos->right_fork);
 		pthread_mutex_lock(philos->print);
 		print_thread(philos, philos->id, PHILO_RELEASE_FORK);
@@ -71,7 +100,6 @@ void	release_forks(t_philo *philos)
 	}
 	else
 	{
-		//usleep(100);
 		pthread_mutex_unlock(philos->left_fork);
 		pthread_mutex_lock(philos->print);
 		print_thread(philos, philos->id, PHILO_RELEASE_FORK);
@@ -81,6 +109,28 @@ void	release_forks(t_philo *philos)
 		printf("%p\n", philos->right_fork);
 		pthread_mutex_unlock(philos->print);
 	}
+/* 	if (philos->id % 2 == 0  && philos->nb_philos % 2)
+	{
+		pthread_mutex_unlock(philos->left_fork);
+		pthread_mutex_lock(philos->print);
+		print_thread(philos, philos->id, PHILO_RELEASE_FORK);
+		printf("%p\n", philos->left_fork);
+		pthread_mutex_unlock(philos->right_fork);
+		print_thread(philos, philos->id, PHILO_RELEASE_FORK);
+		printf("%p\n", philos->right_fork);
+		pthread_mutex_unlock(philos->print);
+	}
+	else
+	{
+		pthread_mutex_unlock(philos->right_fork);
+		pthread_mutex_lock(philos->print);
+		print_thread(philos, philos->id, PHILO_RELEASE_FORK);
+		printf("%p\n", philos->right_fork);
+		pthread_mutex_unlock(philos->left_fork);
+		print_thread(philos, philos->id, PHILO_RELEASE_FORK);
+		printf("%p\n", philos->left_fork);
+		pthread_mutex_unlock(philos->print);
+	} */
 }
 
 int	check_philo(t_philo *philos)
@@ -90,16 +140,11 @@ int	check_philo(t_philo *philos)
 	{
 		pthread_mutex_unlock(philos->dead);
 		return (0);
-	}	
-/* 	if (*philos->dead_flag == 1)
-	{
-		pthread_mutex_unlock(philos->dead);
-		return (0);
-	} */
+	}
 	pthread_mutex_unlock(philos->dead);
 	usleep(100);
 	pthread_mutex_lock(philos->dead);
-	if (get_time() >= philos->time_to_die)
+	if (get_time() >= philos->time_to_die && !*philos->dead_flag)
 	{
 		*philos->dead_flag = 1;
 		pthread_mutex_lock(philos->print);
@@ -114,9 +159,9 @@ int	check_philo(t_philo *philos)
 
 int	eating(t_philo *philos)
 {
-	take_forks(philos);
-	
-	if (!check_philo(philos))
+	if (check_philo(philos) || *philos->dead_flag == 1)
+		take_forks(philos);
+	if (!check_philo(philos) || *philos->dead_flag == 1)
 	{
 		pthread_mutex_unlock(philos->left_fork);
 		pthread_mutex_unlock(philos->right_fork);
@@ -127,11 +172,7 @@ int	eating(t_philo *philos)
 	pthread_mutex_unlock(philos->meal);
 	philos->last_meal = get_time();
 	philos->time_to_die = philos->last_meal + philos->ms_to_die;
-	printf("Time to die: %ld\n", philos->time_to_die);
-	if (philos->time_to_eat + get_time() >= philos->time_to_die)
-		ft_usleep(philos->time_to_die - get_time());
-	else
-		ft_usleep(philos->time_to_eat);
+	ft_usleep(philos->time_to_eat);
 	release_forks(philos);
 	if (philos->times_each_must_eat != -1)
 		philos->times_each_must_eat--;
@@ -140,12 +181,12 @@ int	eating(t_philo *philos)
 
 void	*routine(t_philo *philos)
 {
-	ft_usleep(10);
+	ft_usleep(5);
 	philos->start_time = get_time();
 	philos->last_meal = philos->start_time;
 	philos->ms_to_die = philos->time_to_die;
 	philos->time_to_die = philos->last_meal + philos->ms_to_die;
-	if (philos->id % 2 == 0)
+	if (philos->id % 2 == 1)
 			ft_usleep(philos->time_to_eat);
 	while (check_philo(philos) && *philos->dead_flag == 0)
 	{
@@ -155,12 +196,10 @@ void	*routine(t_philo *philos)
 			print_thread(philos, philos->id, PHILO_DIE);
 			return (NULL);
 		}
-		/* if (philos->id % 2 == 0)
-			ft_usleep(5); */
-		if (!eating(philos))
-		{
-			return (NULL);
-		}
+		if (philos->id % 2 == 0)
+			ft_usleep(3);
+		if (check_philo(philos) || !*philos->dead_flag)
+			eating(philos);
 		if (!sleeping(philos))
 			return (NULL);
 		if (!think(philos))
--- src/tools2.c
+++ src/tools2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   tools2.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: gongarci <gongarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 14:24:21 by marvin            #+#    #+#             */
-/*   Updated: 2025/01/23 00:48:08 by marvin           ###   ########.fr       */
+/*   Updated: 2025/01/26 02:10:23 by gongarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,10 @@ void	print_thread(t_philo *philos, int id, char *str)
 
 int	think(t_philo *philos)
 {
-	if (!check_philo(philos))
+	if (!check_philo(philos) || *philos->dead_flag == 1)
 		return (0);
+	pthread_mutex_lock(philos->print);
 	print_thread(philos, philos->id, PHILO_THINK);
+	pthread_mutex_unlock(philos->print);
 	return (1);
 }
